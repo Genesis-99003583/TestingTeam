0e139b139f50b7bb4a71b05085c2713cd735845c
abhishekreddy012935 <abhishekreddy909@gmail.com>
2021-04-29 17:06:26 +0530

    Added Config File, error handling using decorators

--- Project-with_Classes/bin/main.py
+++ Project-with_Classes/bin/main.py
@@ -16,19 +16,14 @@ if __name__ == '__main__':
 		print("----------------In Main----------------")
 		request = int(input("1. GET\n2. POST\n3. PUT\n4. DELETE\n5. Download Image\n6. Exit\nSelect anything from 1 to 6: "))
 		if(request == 1):
-			# getStudData = GetStudentData(domain, path)
 			studData.getMain()
 		elif(request == 2):
-			# postStudData = PostStudentData(domain, path)
 			studData.postMain()
 		elif(request == 3):
-			# putStudData = PutStudentData(domain, path)
 			studData.putMain()
 		elif(request == 4):
-			# delStud = DeleteStudentData(domain, path)
 			studData.deleteMain()
 		elif(request == 5):
-			# imageData = Image()
 			studData.imageMain()
 		elif(request == 6):
 			exit()
